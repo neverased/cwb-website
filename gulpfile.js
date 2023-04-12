48c6df1ea872f24a4c4964394aeaf42f3f512f85
Wojciech Bajer <mail@wojciechbajer.com>
2023-04-12 17:28:13 +0200

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+var GLOBAL = {
+    dirs: {
+        sass: './src/sass/',
+        pug: './src/pug/',
+        assets: './assets/',
+        css: './assets/css/',
+    },
+    themeName: 'oona'
+};
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function buildSass() {
+    return gulp.src(GLOBAL.dirs.sass + '*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(GLOBAL.dirs.css));
+}
+
+exports.buildSass = buildSass;
+exports.watch = function() {
+    gulp.watch(GLOBAL.dirs.sass + '*.scss', buildSass);
+}
+
+exports.default = gulp.series(buildSass);
